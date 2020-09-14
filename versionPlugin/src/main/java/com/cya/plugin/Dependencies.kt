c99442c8ed030ea907b71f37272c87a4e19faead
troyzqming <tonyzqming@163.com>
2020-09-14 16:19:48 +0800

    ^ 修改config

---
+++ versionPlugin/src/main/java/com/cya/plugin/Dependencies.kt
@@ -0,0 +1,71 @@
+package com.cya.plugin
+
+object BuildAndroid {
+
+    const val CompileSdkVersion = 29
+    const val BuildToolsVersion = "29.0.2"
+    const val MinSdkVersion = 21
+    const val TargetSdkVersion = 29
+    const val VersionCode = 1
+    const val VersionName = "1.0.0"
+}
+
+object Versions {
+    const val Kotlin = "1.4.0"
+    const val Appcompat = "1.2.0"
+    const val Annotation = "1.1.0"
+    const val CoreKtx = "1.3.1"
+    const val ConstraintLayout = "2.0.1"
+    const val ViewPager2 = "1.0.0"
+    const val Recyclerview = "1.1.0"
+    const val Coroutines = "1.3.7"
+    const val Koin = "2.1.5"
+    const val JetPackNavigation = "2.3.0"
+    const val JetPackLifecycleViewModel = "2.2.0"
+    const val JetPackLifecycleLiveData = "2.2.0"
+    const val JetPackPaging = "2.1.2"
+    const val Glide = "4.11.0"
+}
+
+object Dependencies {
+    const val KotlinLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.Kotlin}"
+    const val Appcompat = "androidx.appcompat:appcompat:${Versions.Appcompat}"
+    const val Annotation = "androidx.annotation:annotation:${Versions.Annotation}"
+    const val CoreKtx = "androidx.core:core-ktx:${Versions.CoreKtx}"
+    const val ConstraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.ConstraintLayout}"
+    const val ViewPager2 = "androidx.viewpager2:viewpager2:${Versions.ViewPager2}"
+    const val Recyclerview = "androidx.recyclerview:recyclerview:${Versions.Recyclerview}"
+
+    //协程
+    const val Coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.Coroutines}"
+    const val CoroutinesAndroid =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.Coroutines}"
+
+    //koin
+    const val KoinViewModel = "org.koin:koin-android-viewmodel:${Versions.Koin}"
+
+    //jetpack
+    const val LifecycleViewModel =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.JetPackLifecycleViewModel}"
+    const val LifecycleLiveData =
+        "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.JetPackLifecycleLiveData}"
+    const val Paging = "androidx.paging:paging-runtime:${Versions.JetPackPaging}"
+    const val NavigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.JetPackNavigation}"
+    const val NavigationUI = "androidx.navigation:navigation-ui-ktx:${Versions.JetPackNavigation}"
+    const val NavigationDynamic = "androidx.navigation:navigation-dynamic-features-fragment:${Versions.JetPackNavigation}"
+
+    const val LiveBus = "com.jeremyliao:live-event-bus-x:1.5.7"
+    const val Retrofit = "com.squareup.retrofit2:retrofit:2.7.1"
+    const val RetrofitGson = "com.squareup.retrofit2:converter-gson:2.6.2"
+    const val OkHttpLogging = "com.squareup.okhttp3:logging-interceptor:4.0.0"
+    const val LoggingInterceptor = "com.github.ihsanbal:LoggingInterceptor:3.0.0"
+    const val Logger = "com.orhanobut:logger:2.2.0"
+    const val CheckUpdate = "com.github.AlexLiuSheng:CheckVersionLib:2.4_androidx"
+    const val BaseRecyclerViewAdapterHelper =
+        "com.github.CymChad:BaseRecyclerViewAdapterHelper:2.9.46"
+    const val Glide = "com.github.bumptech.glide:glide:${Versions.Glide}"
+    const val GlideCompiler =
+        "com.github.bumptech.glide:compiler:${Versions.Glide}"
+    const val smartRefreshLayout = "com.scwang.smartrefresh:SmartRefreshLayout:1.1.2"
+}
---
+++ versionPlugin/src/main/java/com/cya/plugin/VersionPlugin.kt
@@ -0,0 +1,11 @@
+package com.cya.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VersionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+    }
+
+    companion object
+}
